a9fa2bcf578a3ab82c5079fabdb086565a932fc4
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-23 22:38:22 +0530

    [Avinash] add a program  to connect the database in employee payroll app

--- src/main/java/com/bridgelabz/MysqlAppliaction/model/Employee.java
+++ src/main/java/com/bridgelabz/MysqlAppliaction/model/Employee.java
@@ -19,6 +19,8 @@ public class Employee {
     private String department;
     private double salary;
 
+
+
     public Employee(String name, String department, double salary) {
         this.name = name;
         this.department = department;
