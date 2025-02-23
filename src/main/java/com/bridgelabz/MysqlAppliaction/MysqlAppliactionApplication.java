58d2c1febd0e73cd6c92359eda311a6d96c1d237
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-23 14:30:39 +0530

    [Avinash] add a empty project of employee payroll app

---
+++ src/main/java/com/bridgelabz/MysqlAppliaction/MysqlAppliactionApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.MysqlAppliaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlAppliactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlAppliactionApplication.class, args);
+	}
+
+}
